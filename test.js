f5da8b6d48b29cb6713686a8cfa90c27108c7e1c
K Gilbert <tordre@hellotoaster.com>
2017-10-20 02:09:55 -0230

    Initial Page File Handling Added
    Sample files added to page, only a static pag WebResponse class has been created, WebResponses can be extended to true server side processes, new Web Responses need to be pushed into the pages array to be added to the page. Might not be the best way if the page gets larger as currently each request requires a linear search.

--- test.js
+++ test.js
@@ -1,17 +1,101 @@
 /**
  * 
  */
-const http = require('http');
+
+var url = require('url'),
+http = require("http"),
+path = require("path"),
+url = require("url"),
+fs = require("fs");
 
 const hostname = '127.0.0.1';
-const port = 3000;
+const port = 8080;
+
+// maps file extention to MIME typere
+const ext_map = {
+  '.ico': 'image/x-icon',
+  '.html': 'text/html',
+  '.js': 'text/javascript',
+  '.json': 'application/json',
+  '.css': 'text/css',
+  '.png': 'image/png',
+  '.jpg': 'image/jpeg',
+  '.wav': 'audio/wav',
+  '.mp3': 'audio/mpeg',
+  '.svg': 'image/svg+xml',
+  '.pdf': 'application/pdf',
+  '.doc': 'application/msword'
+};
+
+class WebResponse {
+	constructor(page) {
+		this.page = page;
+	}
+
+	check_hit(page){
+		return(this.page == page)
+	}
+	response(req, res) {
+	}
+}
+
+class staticWebResponse extends WebResponse{
+
+	constructor(page, file) {
+		super(page);
+		this.file = file;
+
+	}
+	
+	response(req, res){
+		var content_type = ext_map[path.parse(this.file).ext];
+		  fs.readFile(this.file, function(err, data ) {
+			    res.writeHead(200, {'Content-Type': content_type});
+			    res.write(data);
+			    res.end();
+			  })
+	}
+
+}
+
+
+var pages = [];
+pages.push(new staticWebResponse("./style.css","./static/style.css"));
+pages.push( new staticWebResponse("./index.html","./static/index.html"));
+pages.push( new staticWebResponse("./","./static/index.html"));
+pages.push( new staticWebResponse("./user.html","./static/user.html"));
+pages.push( new staticWebResponse("./vote.html","./static/vote.html"));
+pages.push( new staticWebResponse("./party.html","./static/party.html"));
+pages.push( new staticWebResponse("./party_enroll.html","./static/party_enroll.html"));
+pages.push( new staticWebResponse("./new_election.html","./static/new_election"));
+pages.push( new staticWebResponse("./election_results.html","./static/election_results.html"));
+pages.push( new staticWebResponse("./candidate.html","./static/candidate.html"));
+pages.push( new staticWebResponse("./admin.html","./static/admin.html"));
+
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+	var q = url.parse(req.url, true);
+	var filename = "." + q.pathname;
+	
+
+	var hit = false;
+	var pageLength = pages.length;
+	for (i = 0; i < pageLength; i++) {
+		if(pages[i].check_hit(filename)){
+			hit = true;
+
+			pages[i].response(req, res);
+			break;
+		}
+	}
+	if(!hit){
+		res.statusCode = 200;
+		res.setHeader('Content-Type', 'text/plain');
+		res.end('Hello World\n');
+	}
+	
 });
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+	console.log(`Server running at http://${hostname}:${port}/`);
 });
