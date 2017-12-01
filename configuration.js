cd01c9831891d72e2970d70cc50e2dbaab861357
K. Gilbert <tordre@hellotoaster.com>
2017-12-01 01:16:36 -0330

    Exported a few vars into a config file

---
+++ configuration.js
@@ -0,0 +1,13 @@
+var config = {};
+
+config.database = {};
+config.database.host = '127.0.0.1';
+config.database.port = 23346;
+config.database.name = 'election';
+config.database.url = "mongodb://" + config.database.host + ":" + config.database.port + "/"+ config.database.name;
+
+config.web = {};
+config.web.hostname = 'localhost'; 
+config.web.port = 3346;
+
+module.exports = config;
